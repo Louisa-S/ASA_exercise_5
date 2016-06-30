9e3557aeb608c459e454b5333d4bd7d1f219208f
Louisa <Louisaschwed@gmx.de>
2016-06-30 16:49:25 +0200

    Algotithm works including backtracking

--- alignment.py
+++ alignment.py
@@ -1,37 +1,48 @@
 import sys
 
-def  local_al(s, t, maxdist, matrix):
+def  local_al(T, s, t, matrix):
 	
 	m = len(s)
 	n = len(t)
 
 	maxi = (0,0,0) #pos i, pos j, score
 	
-	T = []   # create  empty  matrix
+	  # create  empty  matrix
 	
 	
-	T.append([0] * (m+1))
+	T.append([(0, "l")] * (m+1))
 
 	for i in range(1, n+1):
-		T.append([None] * (m+1)) # add  empty  row i
-		T[i][0] = 0 # init 0th  column  of row i
+		T.append([(None, None)] * (m+1)) # add  empty  row i
+		T[i][0] = (0, "u") # init 0th  column  of row i
 		
 
 		for j in range(1, m+1):
-			T[i][j] = max(0, T[i-1][j-1] + score(t[i-1], s[j-1], matrix), T[i-1][j] -10, T[i][j-1] -10)
-			if T[i][j] > maxi[2]:
-				maxi = (i, j, T[i][j])
-		print T
-	
+			sco = score(t[i-1], s[j-1], matrix)
+			maximum = max(0, T[i-1][j-1][0] + sco, T[i-1][j][0] -10, T[i][j-1][0] -10)
+			ind = [0, T[i-1][j-1][0] + sco, T[i-1][j][0] -10, T[i][j-1][0] -10].index(maximum)
+			T[i][j] = (maximum, getdirection(ind))
+			
+			if T[i][j][0] > maxi[2]:
+				maxi = (i, j, T[i][j][0])
+				
 	
 	return maxi
 
 
 def score(c1, c2, matrix):
 	return int(matrix[getindex(c1)][getindex(c2)])
+
+def getdirection(pos):
+	if pos == 1:
+		return "d"
+	elif pos == 2:
+		return "u"
+	elif pos == 3:
+		return "l"
+	else: return "undef"
 	
-	
-	
+
 	
 def readmatrix(f):
 	matrix = []
@@ -61,10 +72,48 @@ def getindex(c):
 	l = ["A", "R", "N", "D", "C", "Q", "E", "G", "H", "I", "L", "K", "M", "F", "P", "S", "T", "W", "Y", "V", "B", "Z", "X"] 
 	return l.index(c)
 	
+
+def getstring(T, s, t, maxi):
+	i = maxi[0]
+	j = maxi[1]
 	
+	start = T[i][j]
+	ress = ""
+	#ress += s[j]
+	rest = ""
+	#rest += t[i]
+	
+	while(start[0] != 0):
+		
+		if start[1] == "d":
+			i -= 1
+			j -= 1
+			ress += s[j]
+			rest += t[i]
+		elif start[1] == "u":
+			i -= 1
+			ress += "-"
+			rest += t[i]
+		else:
+			j -= 1
+			ress += s[j]
+			rest += "-"
+					
+		start = T[i][j]
+		
+		 
+	return ress[::-1], rest[::-1]
 	
 #main
 with open(str(sys.argv[3])) as f:
 	matrix = readmatrix(f)
-	
-print local_al(sys.argv[1], sys.argv[2], 1000, matrix)
+
+T = []	
+s = sys.argv[1]
+t = sys.argv[2]
+maxi = local_al(T,s , t, matrix)
+print maxi [2]
+
+strings = getstring(T,s, t ,maxi)
+print strings[0]
+print strings[1]
